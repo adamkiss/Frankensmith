6729f755f1dd9a4eed5a8fef3cedc6de5d3b8592
Adam Kiss <iam@adamkiss.com>
2016-03-15 19:15:54 +0100

    …And add the jstransformer-php

---
+++ src/plugins/jstransformer-php/jstransformer-php.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/*
+  Custom JSTransformer so I can skip options and plugins in jade.
+
+  It's a copy from:
+  https://github.com/RobLoach/jstransformer-markdown-it
+
+  Modified by me.
+ */
+
+exports.name = 'php';
+exports.outputFormat = 'php';
+exports.render = function (str, options) {
+  console.log(str,options);
+
+  return '<?php '+str+' ?>';
+};
